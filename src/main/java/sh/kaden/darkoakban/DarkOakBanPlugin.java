ca0e665709cd583aff18b7250110453e3a796c3c
webcrawls <github.com@kaden.sh>
2024-10-14 14:11:37 -0400

    "modern-ify" * switch to gradle as a build tool * delete obsolete code * Literally, Deleted Herobrine * supports 1.21.1 or whatever!

--- src/main/java/sh/kaden/darkoakban/DarkOakBanPlugin.java
+++ src/main/java/sh/kaden/darkoakban/DarkOakBanPlugin.java
@@ -1,18 +1,23 @@
-package sh.kaden.darkoakban.listeners;
+package sh.kaden.darkoakban;
 
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.EventHandler;
 
-public class DarkOakSignListener implements Listener {
+public final class DarkOakBanPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
     @EventHandler
     public void onSign(SignChangeEvent event) {
         Player player = event.getPlayer();
-
         Block block = event.getBlock();
 
         if (block.getType() == Material.DARK_OAK_WALL_SIGN) {
@@ -20,4 +25,8 @@ public class DarkOakSignListener implements Listener {
         }
     }
 
+    @Override
+    public void onDisable() {
+//        HandlerList.unre
+    }
 }
